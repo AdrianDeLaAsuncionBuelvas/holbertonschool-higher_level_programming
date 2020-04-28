8e20a293c6aa00b43b1f22b10f2069cbfbd6e57f
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-04-28 15:17:40 -0500

    add first Task correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -15,5 +15,5 @@ if (num > 5):
     print("is {}".format(num), "and is greater than 5")
 elif (num == 0):
     print("is {}".format(num), "and is 0")
-elif (num < 6):
+elif (num < 6 and num != 0):
     print("is {}".format(num), "and is less than 6 and not 0")
