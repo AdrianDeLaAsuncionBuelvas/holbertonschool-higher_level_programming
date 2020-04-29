1510e196cf21fe8fc7fc6556e680ceed2a181fcf
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-04-28 19:48:17 -0500

    add sixfth Task Correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if (i >= j):
-            continue;
+            continue
         elif (i == 8 and j == 9):
             print("{:d}{:d}".format(i, j))
         else:
