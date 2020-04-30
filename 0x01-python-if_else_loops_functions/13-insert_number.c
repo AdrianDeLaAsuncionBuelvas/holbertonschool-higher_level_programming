8d07f46a44d60d37dea976088a1e2cd2506f98b0
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-04-29 20:30:21 -0500

    add  13 Task add number to new Node correction

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,25 +9,31 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
+	listint_t *new = NULL;
 	listint_t *node = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = number;
 	if (*head == NULL)
 	{
 		*head = new;
 		new->next = NULL;
 		return (new);
 	}
-	while (node != NULL)
+	while (node->next)
 	{
-		node = node->next;
 		if (node->n >= number)
 		{
-			new->n = number;
 			new->next = node;
+			*head = new;
+			return (new);
+		}
+		if ((number >= node->n) && (node->next == NULL))
+		{
+			new->next = NULL;
+			new->next = node->next;
 			return (new);
 		}
 		if ((number >= node->n) && (number <= node->next->n))
