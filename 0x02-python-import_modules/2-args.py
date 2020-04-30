f9710a9cbed8ebc968f4e4fd17fc09f828f3a645
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-04-30 18:22:37 -0500

    add second Task print the number of the list of its Arguments (correction4)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ import sys
 from sys import argv
 if (__name__ == '__main__'):
     if (len(argv) == 1):
-        print("{:d} arguments".format(len(sys.argv) - 1))
+        print("{:d} arguments.".format(len(sys.argv) - 1))
     elif (len(argv) == 2):
-        print("{:d} argument".format(len(sys.argv) - 1))
-    else:
-        print("{:d} arguments".format(len(sys.argv) - 1))
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    elif(len(argv) > 1):
+        print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(argv)):
         print("{:d}: {}".format(i, argv[i]))
