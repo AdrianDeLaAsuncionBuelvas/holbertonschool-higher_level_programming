d90dcac88768c2b1b2ba0099751011f509d16d4a
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-04-30 16:35:21 -0500

    add 0 Task import test 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ from add_0 import add
 a = 1
 b = 2
 res = add(a, b)
-print("{} + {}".format(a, b), "= {}".format(res))
+if(__name__ == '__add_0__'):
+    print("{} + {}".format(a, b), "= {}".format(res))
