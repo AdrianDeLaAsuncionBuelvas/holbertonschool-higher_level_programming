380a0ace11dc08ae763c3f32dc590db810599415
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-01 00:01:20 -0500

    add first Task handles basics operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,7 +15,7 @@ if (__name__ == '__main__'):
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif (operator == '-'):
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif (operator == "*"):
+    elif (operator == '*'):
         print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif (operator == '/'):
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
