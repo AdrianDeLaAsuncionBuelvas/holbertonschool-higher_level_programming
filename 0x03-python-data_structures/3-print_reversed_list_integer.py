2d438eba6525f0af473daba7ef37362e77444d7d
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-05 14:15:44 -0500

    add Third Task print of a list, in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if (my_list):
         for i in my_list[::-1]:
             str = "{:d}"
-            print (str.format(i))
+            print(str.format(i))
