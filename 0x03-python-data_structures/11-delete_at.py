3a5d2bf2340d8af0f936ae848bf805a42c6217b0
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-05 20:37:15 -0500

    add eleventh Task deletes the item at a specific position test 2

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if (idx < 0):
-        return (my_list)
     for i in my_list:
+        if (idx < 0):
+            return (my_list[i])
         if (idx == i):
             del my_list[idx]
     return (my_list)
