f094ccfef1d6bd6d21d6635c1aab0094fccd5e19
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-08 20:47:28 -0500

    git add 12 Task that converts a Roman numeral to an integer. test 8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,14 +3,18 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return (0)
 
-    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    value_previous = 0
-    for i in range(len(roman_string)):
-        for key, value in numbers.items():
-            if (key == roman_string[i]):
-                result += value
-                if (value_previous < value):
-                    result -= (value_previous * 2)
-                    value_previous = value
-    return (result)
+    num = {'I': 1,
+               'V': 5,
+               'X': 10,
+               'L': 50,
+               'C': 100,
+               'D': 500,
+               'M': 1000
+    }
+    res = 0
+    for i in range(0, len(roman_string)):
+        if i == 0 or num[roman_string[i]] <= num[roman_string[i - 1]]:
+                res += num[roman_string[i]]
+        else:
+                res += num[roman_string[i]] - 2 * num[roman_string[i - 1]]
+    return (res)
