c0449db50f27e2548f4dbcae9657ab42f1c771c3
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-19 15:37:13 -0500

    add Second Task Documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,10 @@ class Square:
         """Class Method Private instance attribute"""
         if type(size) is int:
             if size < 0:
+                """Raised when the Value is incorrect"""
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
         else:
+            """Raised when the Value is incorrect"""
             raise TypeError("size must be an integer")
