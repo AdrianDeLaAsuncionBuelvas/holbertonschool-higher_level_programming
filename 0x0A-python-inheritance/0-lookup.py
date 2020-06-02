e33066d86f9b3c5776bf67db542fa10e36bf6041
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-02 18:24:27 -0500

    add 0 Task return the list of available attributes correction

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+function that returns the list of available attributes \
+and methods of an object
+"""
 
 
 def lookup(obj):
     """
-    function that returns the list of available attributes \
-    and methods of an object:
+    lookup - return a list of available attributes
+    obj - object to check
     """
-    return(dir(obj))
+    return (dir(obj))
