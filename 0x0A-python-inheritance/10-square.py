26f142cf7b72e68a2088421bdc8368545df8f284
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-02 21:57:57 -0500

    add 10 Task Class Square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,7 +7,7 @@ Class BaseGeometry
 
 class BaseGeometry():
     """
-    BaseGeometry - Empty Class
+    Class BaseGeometry
     """
     pass
 
@@ -70,4 +70,5 @@ class Square(Rectangle):
 
         self.integer_validator("size", size)
         self.__size = size
+
         super().__init__(size, size)
