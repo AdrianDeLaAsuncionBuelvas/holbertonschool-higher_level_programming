9d180ad559f9f4cc3f4fa11ecf53078f6790eb80
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-02 22:54:36 -0500

    add 8 Task class Rectangle that inherits from BaseGeometry correction2

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,33 +5,6 @@ Class BaseGeometry
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry():
-    """
-    Class that defines a Figure
-    """
-
-    pass
-
-    def area(self):
-        """
-        Area of a Geometry
-        """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Integer_validator - validates if an integers was entered
-        name: string to validate
-        value: Value to validate
-        """
-
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if (value <= 0):
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
 class Rectangle(BaseGeometry):
     """
     Class that inherits from BaseGeometry
