2ea6bc28cede8f8db61ca5a5e2e508d95e6728da
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-02 20:30:37 -0500

    add 7 Task and test cases Integer Validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry - Empty Class
+    """
+    def area(self):
+        """
+        Area of a Geometry
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        integer_validator - validates if an integers was entered
+        """
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{:s} must be greater than 0".format(name))
