a5b85eca47d0cd531c91ea64bfd5a11203fe20c7
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-03 23:00:32 -0500

    add 11 Task class Student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Class that define a Student
+"""
+
+
+class Student():
+    """
+    Define a Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initialization Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student
+        """
+        return (self.__dict__)
