b516843b611204abe2a743d4ee51a12d5af2121d
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-03 22:50:58 -0500

    add 10 Task

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that returns the dictionary description with simple data structure \
+(list, dictionary, string, integer and boolean) \
+for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """
+    class_to_json - returns the dictionary description with \
+simple data structure
+    obj - is an instance of a class
+    """
+
+    return (obj.__dict__)
