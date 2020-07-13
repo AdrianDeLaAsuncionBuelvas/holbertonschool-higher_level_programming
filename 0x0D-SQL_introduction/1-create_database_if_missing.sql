e228b32d247a00deff705cbba33675fae475af9f
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-13 14:26:56 -0500

    add 1 Task Create a Database and delete unnecesary part

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
-#!/usr/bin/env mysql
 -- script that creates the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
