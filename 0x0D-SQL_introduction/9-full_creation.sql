9db35d199662b807e61fcb37a2f6edea72fb34c4
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-13 17:16:55 -0500

    add 9 Task create table second table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- script that creates a table second_table in the database hbtn_0c_0.
 
 CREATE TABLE IF NOT EXISTS seccond_table (
-       id INT AUTO_INCREMENT,
+       id INT,
        name VARCHAR(256),
        score INT
 );
