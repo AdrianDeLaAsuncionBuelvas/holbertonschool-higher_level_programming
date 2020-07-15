c066c21ac80c187910d39e63a647c4b5991676fb
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-14 22:26:32 -0500

    add Task 0 lists all privileges of the MySQL try 10

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges user_1 and user_2.
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
