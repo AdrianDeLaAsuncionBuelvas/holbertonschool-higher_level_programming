1e4a6a6d41b663ff91d8260b38fb402c12c3d6ed
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-07-14 22:02:33 -0500

    add List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -7,5 +7,5 @@ INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name  IS NULL OR tv_genres.name IS NOT NULL
+WHERE tv_genres.name IS NULL OR tv_genres.name IS NOT NULL
 ORDER BY title ASC, name;
