11a247138f65e6634718a29af911c4187bb00448
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 09:31:00 -0500

    add 1 Task fixing comments

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Write a script that lists all states from the database hbtn_0e_0_usa.
+""" Write a script that lists all states from the database hbtn_0e_0_usa."""
 
 import MySQLdb
 import sys
@@ -9,11 +9,13 @@ if __name__ == '__main__':
     password = sys.argv[2]
     db_Name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_Name)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_Name
+    )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
     ORDER BY id ASC")
