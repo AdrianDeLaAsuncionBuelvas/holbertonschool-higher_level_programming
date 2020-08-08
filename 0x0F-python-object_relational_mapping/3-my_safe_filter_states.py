fa17deeb4a3b9beb86383cd9842b913743d0e5d6
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 09:40:33 -0500

    add 3 Task SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-# write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!
+""" write a script that takes in arguments and displays all values in the \
+states table of hbtn_0e_0_usa
+ where name matches the argument. But this time, \
+write one that is safe from MySQL injections! """
 
 import MySQLdb
 import sys
-
 
 if __name__ == '__main__':
     username = sys.argv[1]
@@ -11,20 +13,20 @@ if __name__ == '__main__':
     db_Name = sys.argv[3]
     state_Name = sys.argv[4]
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=db_Name
-)
-cur = db.cursor()
-sql = ("SELECT * FROM states WHERE name = %s;")
-cur.execute(sql, (state_Name,))
-rows = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_Name
+    )
+    cur = db.cursor()
+    sql = ("SELECT * FROM states WHERE name = %s;")
+    cur.execute(sql, (state_Name,))
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
