c1933c99aeaabc4f94cc3f73d9be6b7f1e093171
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 09:37:24 -0500

    add 2 Task fixing comments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
-# Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+#!/usr/bin/python3
+""" Write a script that takes in an argument and displays all values in the \
+states table of hbtn_0e_0_usa where name matches the argument."""
 
 import MySQLdb
 import sys
@@ -11,20 +12,20 @@ if __name__ == '__main__':
     db_Name = sys.argv[3]
     state_Name = sys.argv[4]
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=db_Name
-)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-ORDER BY id ASC".format(state_Name))
-rows = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_Name
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+    ORDER BY id ASC".format(state_Name))
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
