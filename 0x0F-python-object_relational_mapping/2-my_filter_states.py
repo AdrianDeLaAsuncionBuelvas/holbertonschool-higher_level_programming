9c79018f5bbd3df48fe10a8dbde0154d6cfdf2d1
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-07 20:48:21 -0500

    add 2 Task Filter States by User input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Write a script that lists all states from the database hbtn_0e_0_usa.
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_Name = sys.argv[3]
+    state_Name = sys.argv[4]
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=db_Name
+)
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+ORDER BY id ASC".format(state_Name))
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+db.close()
