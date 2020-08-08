63bfebe1c703935bd631bd8ffe10cb00df7e28fc
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 09:11:45 -0500

    add 0 Task try to correct 2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # Write a script that lists all states from the database hbtn_0e_0_usa.
 # Your script should take 3 arguments: mysql username, mysql password and database name
 
