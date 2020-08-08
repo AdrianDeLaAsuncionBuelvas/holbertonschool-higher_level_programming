a1fa791921b60d16e2e87068e027f37fb5618cb3
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 09:24:39 -0500

    add 1 Task fixing identation 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,13 +9,11 @@ if __name__ == '__main__':
     password = sys.argv[2]
     db_Name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_Name
-    )
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_Name)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
     ORDER BY id ASC")
