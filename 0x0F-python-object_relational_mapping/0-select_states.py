0a217404c4e688359576c57d79dfcd2f435c75e3
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 09:29:07 -0500

    add 0 Task try to correct 4

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Write a script that lists all states from the database hbtn_0e_0_usa.
-# Your script should take 3 arguments:
-# mysql username, mysql password and database name
+"""Write a script that lists all states from the database hbtn_0e_0_usa.
+ Your script should take 3 arguments: \
+mysql username, mysql password and database name"""
 
 import MySQLdb
 import sys
@@ -11,20 +11,20 @@ if __name__ == '__main__':
     password = sys.argv[2]
     db_Name = sys.argv[3]
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=db_Name,
-    charset='utf8'
-)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_Name,
+        charset='utf8'
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
