99709d5d4eeec5da46cde16a5836520a11f9e2a0
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 09:47:13 -0500

    add 5 Task all cities by state fixing comments

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-# Write a script that takes in the name of a state as an argument and
-# lists all cities of that state, using the database hbtn_0e_4_usa
+#!/usr/bin/python3
+""" Write a script that takes in the name of a state as an argument and
+ lists all cities of that state, using the database hbtn_0e_4_usa """
 
 import MySQLdb
 import sys
@@ -12,22 +12,22 @@ if __name__ == '__main__':
     db_Name = sys.argv[3]
     state_Name = sys.argv[4]
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=db_Name
-)
-cur = db.cursor()
-cur.execute("SELECT cities.name "
-            "FROM cities INNER JOIN states "
-            "ON states.id = cities.state_id "
-            "WHERE states.name = %s "
-            "ORDER BY cities.id ASC;", (state_Name,))
-rows = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_Name
+    )
+    cur = db.cursor()
+    cur.execute("SELECT cities.name "
+                "FROM cities INNER JOIN states "
+                "ON states.id = cities.state_id "
+                "WHERE states.name = %s "
+                "ORDER BY cities.id ASC;", (state_Name,))
+    rows = cur.fetchall()
 
-print(", ".join([row[0] for row in rows]))
+    print(", ".join([row[0] for row in rows]))
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
