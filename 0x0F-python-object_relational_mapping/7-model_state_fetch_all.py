4172fdf26ff358cd1c1a622d4397d5b6476916b0
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 19:37:50 -0500

    add 7 Task All states via SQLAlchemy fixing comments

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
-# Write a script that lists all State objects from the database hbtn_0e_6_usa
+#!/usr/bin/python3
+"""Write a script that lists all State objects from the database \
+hbtn_0e_6_usa"""
 
 from model_state import Base, State
 import sys
@@ -14,11 +15,11 @@ if __name__ == "__main__":
         sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
 
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
 
-session.close()
+        session.close()
