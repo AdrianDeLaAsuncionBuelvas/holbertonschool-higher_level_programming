3c0c6242d45c54e05cd4855f345076fdf535f108
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-08 19:50:04 -0500

    add 8 Task first state fixing comments

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-# script that prints the first State object from the database hbtn_0e_6_usa
+#!/usr/bin/python3
+""" script that prints the first State object from the database hbtn_0e_6_usa"""
 
 from model_state import Base, State
 import sys
@@ -14,14 +14,14 @@ if __name__ == "__main__":
         sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
 
-req = session.query(State).order_by(State.id).first()
-if req:
-    print("{}: {}".format(1, req.name))
-else:
-    print("Nothing")
+    req = session.query(State).order_by(State.id).first()
+    if req:
+        print("{}: {}".format(1, req.name))
+    else:
+        print("Nothing")
 
-session.close()
+    session.close()
