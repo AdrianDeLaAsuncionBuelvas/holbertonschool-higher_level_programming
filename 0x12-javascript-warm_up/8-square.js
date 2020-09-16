897417b35f8cad35fc8e5ddb4825d240b489c10c
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-15 20:42:32 -0500

    Add Task 8 Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,18 +4,13 @@ let i, j;
 let newstr = '';
 
 if (process.argv.length <= 2) {
-  console.log('Missing size');
+} else if (process.argv[2] < 0) {
+  console.log();
 } else {
   for (i = 0; i < process.argv[2]; i++) {
-    if (i < 0) {
-      break;
-    }
     newstr += 'X';
   }
   for (j = 0; j < process.argv[2]; j++) {
-    if (j < 0) {
-      break;
-    }
     console.log(newstr);
   }
 }
