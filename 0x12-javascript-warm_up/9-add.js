dc8ae7cbba6940956cce8ba3a452a94db077473a
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-15 20:57:04 -0500

    Adding Task 9 Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const first = Number(process.argv[2]);
 const second = Number(process.argv[3]);
-if (isNaN(fisrt) || isNaN(second)) {
-    console.log('NaN');
+if (isNaN(first) || isNaN(second)) {
+  console.log('NaN');
 } else {
-    console.log(add(first, second));
+  console.log(add(first, second));
 }
 
 function add (a, b) {
-    return a + b;
+  return a + b;
 }
