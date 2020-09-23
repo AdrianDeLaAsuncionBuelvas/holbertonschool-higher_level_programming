2b7f41a73a1cdcd9180dcb6efd0a8e8a73338066
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-23 00:08:22 -0500

    Add Task 7 Who was playing in this movie? try 2

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,15 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request.get(url, function (err, res, body) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  const characters = JSON.parse(body).characters;
-  for (let i = 0; i < characters.length; i++) {
-    request.get(characters[i], function (err, res, body) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const name = JSON.parse(body).name;
-      console.log(name);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     });
   }
-})
+});
